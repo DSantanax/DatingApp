67326437e8cdfa5cf010fbca145a12101701919b
Daniel Santana <Santana.danny4g@gmail.com>
2021-12-06 00:18:05 -0800

    Updated user entity to include password and added registration endpoint with DTO

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using API.Data;
+using API.DTOs;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly DataContext _context;
+       public AccountController(DataContext context)
+        {
+            _context = context;
+        }
+        // attr. binds the params from the method to the data
+        [HttpPost("register")]
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+        {
+            // Check if username already exists in db
+            if (await UserExists(registerDto.Username))
+            {
+                return BadRequest("Username is taken.");
+            }
+            using var hmac = new HMACSHA512();
+
+            // Hash takes in and outputs bytes[]
+            var user = new AppUser()
+            {
+                // Lowercase username
+                UserName = registerDto.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+                PasswordSalt = hmac.Key
+            };
+            // Keep tracking of user in state
+            _context.Add(user);
+            // Add to DB async.
+            await _context.SaveChangesAsync();
+            return user;
+        }
+        // Check if username already exists
+        private async Task<bool> UserExists(string username)
+        {
+            return await _context.Users.AnyAsync(user => user.UserName == username.ToLower());
+        }
+    }
+}
